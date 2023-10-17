f1c94b6e0ccc9a637e3b7a0f0825b0fc28676790
Noor Alrai <ameinnoor91@gmail.com>
2023-10-17 14:00:06 +0300

    Done

--- index.js
+++ index.js
@@ -1,45 +1,32 @@
 const example = [
   ["A", "A", "A"],
-  ["A", "A", "A"],
   ["A", "B", "A"],
+  ["A", "A", "A"],
 ];
 
 const whereIsWaldo = (matrix) => {
-  let uniqueElement;
-  let uniqueElementRow;
-  let uniqueElementCol;
-  // Count the occurrences of each element
-  const elementCounts = {};
-
-  for (let row = 1; row <= matrix.length - 1; row++) {
-    for (let col = 1; col <= matrix[row].length - 1; col++) {
-      const element = matrix[row][col];
-      if (elementCounts[element] === undefined) {
-        elementCounts[element] = 1;
-      } else {
-        elementCounts[element]++;
-      }
+ let waldo;
+ let countWaldo={};
+ for (let row=0 ; row< matrix.length ; row++){
+  for(let col = 0 ; col < matrix[row].length ; col++){
+   waldo = matrix[row][col];
+    if(countWaldo[waldo]){
+      countWaldo[waldo]++
+    }
+    else{
+      countWaldo[waldo]=1
     }
   }
+ }
+ for(let row = 0 ; row < matrix.length; row++)
+ for (let col=0 ; col< matrix[row].length ; col++){
+ waldo = matrix[row][col]
+ if(countWaldo[waldo] === 1){
 
-  for (const element in elementCounts) {
-    if (elementCounts[element] === 1) {
-      uniqueElement = element;
-      break;
-    }
-  }
-
-  for (let row = 1; row <= matrix.length - 1; row++) {
-    for (let col = 1; col <= matrix[row].length - 1; col++) {
-      if (matrix[row][col] === uniqueElement) {
-        uniqueElementRow = row + 1;
-        uniqueElementCol = col + 1;
-        break;
-      }
-    }
-  }
-
-  return [uniqueElementRow, uniqueElementCol];
-};
-
-console.log(whereIsWaldo(example));
+  return [row + 1, col + 1]
+ }
+ }
+return null}
+;
+console.log(whereIsWaldo(example))
+const result = whereIsWaldo(example)
